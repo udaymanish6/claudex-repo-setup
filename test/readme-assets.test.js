b69784d4a10f79cad3c5b891480f9d53037ccc48
uday <udaymanish.thumma@gmail.com>
2026-06-18 22:11:06 -0400

    fix: use release package tarball install

--- test/readme-assets.test.js
+++ test/readme-assets.test.js
@@ -23,7 +23,7 @@ test('README links visual assets and GitHub release install', () => {
   }
   assert.match(readme, /github\/v\/release\/udaymanish6\/claudex-repo-setup/);
   assert.match(readme, /install-GitHub_release/);
-  assert.match(readme, /https:\/\/github\.com\/udaymanish6\/claudex-repo-setup\/archive\/refs\/tags\/v1\.0\.3\.tar\.gz/);
+  assert.match(readme, /https:\/\/github\.com\/udaymanish6\/claudex-repo-setup\/releases\/download\/v1\.0\.4\/create-claudex-1\.0\.4\.tgz/);
   assert.doesNotMatch(readme, new RegExp('create-claudex' + '@latest'));
   assert.doesNotMatch(readme, new RegExp('npm create ' + 'claudex'));
   assert.doesNotMatch(readme, /www\.npmjs\.com\/package\/create-claudex/);
